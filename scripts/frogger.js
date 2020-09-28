3c2c8ebe7f900ec90f22bf8299c39cd6aca6aa62
Sean Nemes <snemes@udel.edu>
2020-09-28 13:56:42 -0400

    Constrain the frog to the gameboard. Not fully scalable.

--- scripts/frogger.js
+++ scripts/frogger.js
@@ -1,38 +1,34 @@
+var gameBoardWidth = 60 * 15;
+var gameBoardHeight = 60 * 13;
+
 var froggerGame = function() {
   var self = this;
   this.frog = undefined;
   this.updateInterval = undefined;
 
+  // update the game state and call update functions of other objects
   this.update = function() {
-      $('#frog').css("top",self.frog.yPos+"px");
-      $('#frog').css("left",self.frog.xPos+"px");
-      console.log(self.frog.xPos);
-      console.log(self.frog.yPos);
+    self.frog.update();
   }
 
   this.initialize = function() {
       self.frog = new frog(0,0);
-      console.log("test");
       $(document).keydown(function(event){
           if(event.code=="ArrowUp") {
               event.preventDefault();
-              self.frog.yPos -= self.frog.speed;
-              console.log("up");
+              self.frog.move(0,1);
           }
           else if(event.code=="ArrowDown") {
               event.preventDefault();
-              self.frog.yPos += self.frog.speed;
-              console.log("down");
+              self.frog.move(0,-1);
           }
           else if(event.code=="ArrowLeft") {
               event.preventDefault();
-              self.frog.xPos -= self.frog.speed;
-              console.log("left");
+              self.frog.move(-1,0);
           }
           else if(event.code=="ArrowRight") {
               event.preventDefault();
-              self.frog.xPos += self.frog.speed;
-              console.log("right");
+              self.frog.move(1,0);
           }
       });
 
@@ -47,4 +43,34 @@ var frog=function(x,y) {
   this.xPos = x;
   this.yPos = y;
   this.speed = 60;
+
+  this.update = function() {
+    // enforce gameboard bounds
+    // assumes (0,0) is the bottom center
+    if(self.xPos < -gameBoardWidth/2 + 30) {
+        self.xPos = -gameBoardWidth/2 + 30;
+    }
+    else if(self.xPos > gameBoardWidth/2 - 30) {
+        self.xPos = gameBoardWidth/2 - 30;
+    }
+
+    if(self.yPos < 0) {
+        self.yPos = 0;
+    }
+    else if(self.yPos > gameBoardHeight-60) {
+        self.yPos = gameBoardHeight-60;
+    }
+
+    $('#frog').css("top",-self.yPos+"px"); //note the negative sign; necessary so up is up and down is down; for some reason using the css property "bottom" didn't work right
+    $('#frog').css("left",self.xPos+"px");
+    console.log(self.xPos);
+    console.log(self.yPos);
+  }
+
+  // moves the frog by speed * each multiplier passed in
+  // example: move(1,0) moves the frog [speed] to the right
+  this.move = function(xMult,yMult) {
+    self.xPos += self.speed * xMult;
+    self.yPos += self.speed * yMult;
+  }
 }
